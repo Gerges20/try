c7a1a22fa791d2b0319d88a0484f38c6309b51a3
Gerges20 <66284756+Gerges20@users.noreply.github.com>
2025-07-18 17:48:56 +0300

    Update and rename Downloads.bat to s/Downloads.bat

--- s/Downloads.bat
+++ s/Downloads.bat
@@ -1,24 +1,24 @@
 @echo off
 
-:: ╩▌┌эс RDP
+:: ├К├Э├Ъ├н├б RDP
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
 netsh advfirewall firewall set rule group="Remote Desktop" new enable=Yes
 
-:: ┼┌╧╟╧ ╟с╚╟╙ц╤╧
+:: ├Е├Ъ├П├З├П ├З├б├И├З├У├ж├С├П
 set password=@#Disala123456
 powershell -Command "Set-LocalUser -Name 'runneradmin' -Password (ConvertTo-SecureString -AsPlainText '%password%' -Force)"
 
-:: ╟╙╩╬╤╟╠ IP
+:: ├З├У├К├О├С├З├М IP
 for /f "tokens=2 delims=:" %%a in ('ipconfig ^| findstr /i "IPv4" ^| findstr /v "169.254"') do set myip=%%a
 set myip=%myip:~1%
 
-:: ╩┌ээф ╟╙у ╟су╙╩╬╧у
+:: ├К├Ъ├н├н├д ├З├У├г ├З├б├г├У├К├О├П├г
 set myuser=runneradmin
 
-:: ═▌┘ ╟с╚э╟ф╟╩ ▌э ус▌
+:: ├Н├Э├Щ ├З├б├И├н├З├д├З├К ├Э├н ├г├б├Э
 echo IP Address : %myip% > rdp-info.txt
 echo Username   : %myuser% >> rdp-info.txt
 echo Password   : %password% >> rdp-info.txt
 
-:: ┌╤╓ ╟сус▌ ╚┌╧ ╟с┼ф╩х╟┴
+:: ├Ъ├С├Ц ├З├б├г├б├Э ├И├Ъ├П ├З├б├Е├д├К├е├З├Б
 notepad rdp-info.txt
